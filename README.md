ec8b1fc71a8463cfc2f93157dda7327af1a32030
a7777611 <gianna_611@163.com>
2025-04-28 18:12:39 +0800

    complete the tuple_space class with three operations and a method

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # COMPX234-A3
 System and Network lab3
+Yingheng YU 20233006333
---
+++ tuple_space.py
@@ -0,0 +1,74 @@
+import threading
+from time import time
+
+class TupleSpace:
+    def __init__(self):
+        # a dictionary to store key-value pairs
+        self.tuples = {}
+        # a threading lock to ensure multithreading security
+        self.lock = threading.Lock()
+        # a dictionary to record the static info
+        self.stats ={
+            'total_clients':0,
+            'total_operation':0,
+            'total_reads':0,
+            'total_gets':0,
+            'total_puts':0,
+            'total_errors':0,
+            'start_time':time()
+        }
+    # read(k)
+    def read(self,key):
+        with self.lock:
+            self.stats['total_operations'] +=1
+            self.stats['total_reads'] +=1
+            if key in self.tuples:
+                return f"OK ({key},{self.tuples[key]}"
+            else:
+                self.stats['total_errors'] +=1
+                return f"ERR {key} does not exist"
+    # get(k)
+    def get(self,key):
+        with self.lock:
+            self.stats['total_operations'] +=1
+            self.stats['total_gets'] +=1
+            if key in self.tuples:
+                value = self.tuples.pop(key)
+                return f"OK ({key}, {value}) removed"
+            else:
+                self.stats['total_errors'] +=1
+                return f"ERR {key} does not exist"
+    # put(k,v)        
+    def put(self,key,value):
+        with self.lock:
+            self.stats['total_operations'] +=1
+            self.stats['total_puts'] +=1
+            if key not in self.tuples:
+                self.tuples[key] = value
+                return f"OK ({key}, {value}) added"
+            else:
+                self.stats['total_errors'] += 1
+                return f"ERR {key} already exists"
+    
+    def total_client(self):
+        with self.lock:
+            self.stats['toatl_clients'] +=1
+    
+    def get_stats(self):
+        with self.lock:
+            num_tuples = len(self.tuples)
+            stats = {
+                'num_tuples':num_tuples,
+                **self.stats
+            }
+
+            total_key_size =sum(len(k) for k in self.tuples.keys()) 
+            total_value_size = sum(len(v) for v in self.tuples.values())
+
+            stats.update({
+                'avg_tuple_size': (total_key_size + total_value_size) / num_tuples,
+                'avg_key_size': total_key_size / num_tuples,
+                'avg_value_size': total_value_size / num_tuples
+            })
+        
+        return stats
